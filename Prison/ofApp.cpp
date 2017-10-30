07c7148b3c9a41084d4655f64cc55d42266b3c11
USER1 <vty022620@gmail.com>
2017-10-30 13:16:55 +0900

    手紙

--- Prison/ofApp.cpp
+++ Prison/ofApp.cpp
@@ -1,104 +1,82 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup() {
-    isMove = false;
-    ofSetFrameRate(60);
-    
-    town.load("sky.png");
-    town2.load("sky.png");
-    town3.load("クローバー.png");
-    
-    px = 0;
-    py = 0;
-    
-    mPosition = ofVec2f(px, py);
-    
-    vx = 0;
-    vy = 0;
-    mVelocity = ofVec2f(vx, vy);
+void ofApp::setup(){
+    ofBackground(0,0,0);
+    ofSetVerticalSync(true);
+    frameByframe = false;
+    ofEnableSmoothing();
+    challenge_Movie.loadMovie("challenge_Movie.m4v");
+    challenge_Movie.play();
 }
 
 //--------------------------------------------------------------
-void ofApp::update() {
-    if (isMove == true) {
-        mVelocity.y += 0.01;
-        mPosition += mVelocity;
-    }
-    
-    if (mPosition.y >= ofGetHeight()) {
-        mPosition -= mVelocity;
-    }
+void ofApp::update(){
+   
+    challenge_Movie.update();
 }
 
 //--------------------------------------------------------------
-void ofApp::draw() {
-    
-    town.draw(mPosition.x, mPosition.y - 1100, ofGetWidth(), ofGetHeight());
-    town2.draw(mPosition.x, mPosition.y - 480, ofGetWidth(), ofGetHeight());
-    town3.draw(mPosition.x, mPosition.y , ofGetWidth(), ofGetHeight());
-    
-    /*if (mPosition.y >= 680) {
-     mPosition.y = 100;
-     }*/
-    
+void ofApp::draw(){
+    ofSetColor(0xFFFFFF);
+    challenge_Movie.draw(0, 0, ofGetWidth(), ofGetHeight());
+    ofSetHexColor(0x000000);
+    //ofPixels & pixels = fingersMovie.getPixels();
+    //unsigned char * pixels = fingersMovie.getPixels();
     
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key) {
-    
-    if (key == 'b') {
-        isMove = true;
-    }
+void ofApp::keyPressed(int key){
+
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key) {
-    
+void ofApp::keyReleased(int key){
+
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y) {
-    
+void ofApp::mouseMoved(int x, int y ){
+
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button) {
-    
+void ofApp::mouseDragged(int x, int y, int button){
+
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button) {
-    
+void ofApp::mousePressed(int x, int y, int button){
+
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button) {
-    
+void ofApp::mouseReleased(int x, int y, int button){
+
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y) {
-    
+void ofApp::mouseEntered(int x, int y){
+
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y) {
-    
+void ofApp::mouseExited(int x, int y){
+
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h) {
-    
+void ofApp::windowResized(int w, int h){
+
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg) {
-    
+void ofApp::gotMessage(ofMessage msg){
+
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo) {
-    
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
 }
